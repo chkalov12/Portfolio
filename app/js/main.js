5a3cdeeef794423b118b2f9d6c7f9b4c12cd64bd
chkalov12 <chkalov007@gmail.com>
2021-02-17 21:00:08 +0200

    header

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+    //  $('.header__btn').click(function(){
+    //  	$('html, body').animate({scrollTop: $(document).height() - $(window).height()}, 15000);
+    //  	return false;
+    //    });
+
+
+});
