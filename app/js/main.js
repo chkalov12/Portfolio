0408d6e128ce5d9815a147e6e883dace3652e084
chkalov12 <chkalov007@gmail.com>
2021-03-04 22:51:46 +0200

    add resum

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
 $(function() {
 
-    $('.about__btn')
+    $('.preloader').fadeOut('slow');
+
+    $('.top__btn')
         .on('mouseenter', function(e) {
             var parentOffset = $(this).offset(),
                 relX = e.pageX - parentOffset.left,
@@ -13,6 +15,27 @@ $(function() {
                 relY = e.pageY - parentOffset.top;
             $(this).find('span').css({ top: relY, left: relX })
         });
+
+    $('.btn-details').magnificPopup({
+        type: 'inline',
+        midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });
+
+
+    var wow = new WOW({
+        boxClass: 'wow', // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset: 120, // distance to the element when triggering the animation (default is 0)
+        mobile: true, // trigger animations on mobile devices (default is true)
+        live: true, // act on asynchronously loaded content (default is true)
+        callback: function(box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null // optional scroll container selector, otherwise use window
+    });
+
+    new WOW().init();
 
 
 });
